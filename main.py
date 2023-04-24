1d119934ef8a7c5e023aa96fa87960bc16198116
G3 <purep@qq.com>
2023-04-24 12:12:46 +0800

    实现超级神器活动自动打开QQ登录新页面，未实现点击登录

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class qiang:
     def __init__(self):
-        self.url = "https://cf.qq.com/"
+        self.url = "https://cf.qq.com/cp/a20230308rlr/lr/index.shtml"
         self.driver = webdriver.Edge('/path/to/edgedriver')
+        self.is_chaojishenqi = False
+        # 判断是否是超级神器，包含/lr
+        if '/lr' in self.url:
+            self.is_chaojishenqi = True
 
     def _shut_down_pop_video(self):
         # 找到弹出的视频
@@ -37,8 +41,11 @@ class qiang:
 
     def _login(self):
         # 点击登录按钮
-        login_button = self.driver.find_element(By.ID, "dologin")
-        login_button.click()
+        try:
+            login_button = self.driver.find_element(By.ID, "dologin")
+            login_button.click()
+        except selenium.common.exceptions.ElementClickInterceptedException:
+            print('直接弹出登录窗口，无需点击登录按钮')
 
         # 登录页面是表单内嵌的页面，需要定位到iframe元素，通过id来定位
         login_element = self.driver.find_element(By.ID, "loginIframe")
@@ -46,16 +53,48 @@ class qiang:
         # 使用switch_to.frame()方法切换到该iframe中
         self.driver.switch_to.frame(login_element)
 
-        try:
-            # 点击QQ头像，一定要登录QQ！
-            elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'qlogin_list')))
-            # 执行JavaScript代码点击
-            self.driver.execute_script("document.querySelector('.face').click()")
-            print('登录成功！')
-        except selenium.common.exceptions.TimeoutException:
-            print(f'无弹出视频或视频出现时间超过秒')
+        # 点击QQ头像，一定要登录QQ！
+        # elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'qlogin_list')))
+        # 执行JavaScript代码点击
+        self.driver.execute_script("document.querySelector('.face').click()")
+        print('登录成功！')
 
+        # 切换回去
         self.driver.switch_to.default_content()
+
+    def _open_new_window(self, new_window_url):
+        # 获取当前窗口的句柄
+        current_window_handle = self.driver.current_window_handle
+
+        # 执行JavaScript代码，打开新的窗口并跳转到指定URL
+        self.driver.execute_script("window.open(arguments[0]);", new_window_url)
+
+        # 获取所有窗口的句柄
+        window_handles = self.driver.window_handles
+
+        # 在新打开的窗口中进行操作
+        for handle in window_handles:
+            if handle != current_window_handle:
+                self.driver.switch_to.window(handle)
+                # 在新窗口中进行操作
+                # ...
+
+        # 切回原来的窗口
+        self.driver.switch_to.window(current_window_handle)
+
+    def _chaojishenqi_login(self):
+        # 超级神器登录需要，先切换frame，再打开登录链接登录
+        qqLoginFrame = self.driver.find_element(By.CLASS_NAME, 'qqLoginFrame')
+        qqLoginUrl = qqLoginFrame.get_attribute('src')
+        # 开一个新页面，登录qq
+        self._open_new_window(qqLoginUrl)
+        # qqLoginDriver.get(qqLoginUrl)
+        # # 需要等一段时间
+        # time.sleep(10)
+        # qqLoginDriver.execute_script("document.querySelector('.face').click()")
+        # qqLoginDriver.close()
+
+        self.driver.refresh()
 
     def _init(self):
 
@@ -68,14 +107,22 @@ class qiang:
         self._shut_down_pop_video()
 
         # 登录
+        if self.is_chaojishenqi:
+            self._chaojishenqi_login()
         self._login()
 
-        # if self.driver.find_element_by_id()
+        # 关闭弹出视频
+        self._shut_down_pop_video()
+        time.sleep(2)
+        self._shut_down_pop_video()
 
-        time.sleep(999999)
+    def _get(self):
+        pass
+        # self.driver.execute_script("document.querySelector('tab6 .dia-close').click()")
 
     def run(self):
         self._init()
+        self._get()
 
 
 if __name__ == '__main__':
