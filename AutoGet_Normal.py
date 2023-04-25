1da9953f7baeb6eaa00563e5eab8c744ba0bdd21
G3 <purep@qq.com>
2023-04-25 11:50:22 +0800

    实现普通页面点击领取按钮，上一个版本的今天试用发现不行

--- AutoGet_Normal.py
+++ AutoGet_Normal.py
@@ -57,11 +57,21 @@ class AutoGet_Normal(AutoGet):
         self.driver.execute_script("document.querySelector('.nvabox2 .sp.nav4').click()")
 
     def _get(self):
-        # 等待a标签出现
+        # <div class="p4box1 flex">
+        #     <div class="prop-item">
+        #         <p>M200-赤血龙魂</p>
+        #         <a href="javascript:DM.df.getClick1('933316','','','','2');" class=...
+        #     </div>
+        # </div>
+        # 如抢赤血龙魂
+
+        # 等待p4box1元素出现
         wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.presence_of_element_located(
-            (By.XPATH,
-             '//div[@class="p4box1"]/a[@href="javascript:DM.df.getClick1(\'933316\',\'\',\'\',\'\',\'2\');"]')))
+        p4box1 = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'p4box1')))
+
+        # 在p4box1元素中查找具有特定href属性值的a标签
+        # 注意：在XPath表达式中，我将//更改为.//，以确保只在p4box1元素的子元素中查找a标签。
+        a = p4box1.find_element(By.XPATH, './/a[@href="javascript:DM.df.getClick1(\'933316\',\'\',\'\',\'\',\'2\');"]')
 
         # 点击a标签
-        element.click()
+        a.click()
