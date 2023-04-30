4dc1286acc999e65cf7f9e861793c83bbf5aeb92
G3 <purep@qq.com>
2023-04-30 23:19:20 +0800

    实现五一两个活动的并行领取

--- real_events/e2023_5/main_multi.py
+++ real_events/e2023_5/main_multi.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import threading
 
 from utils import get_time
@@ -5,18 +6,32 @@ from utils import get_time
 import real_events.e2023_5.FuLiFengBao_TianShiWanOu as work1
 import real_events.e2023_5.FuLiFengBao_QBZ as work2
 
-
 def run1():
     x = work1.mod("https://cf.qq.com/", get_time.get_next_minute())
-    x.run_qiang_once(need_time=True)
-
+    x.run()
 
 def run2():
     x = work2.mod("https://cf.qq.com/", get_time.get_next_minute())
-    x.run_qiang_once(need_time=True)
+    x.run()
 
 
-if __name__ == '__main__':
+def multi_process():
+    # 创建进程
+    processes = [
+        multiprocessing.Process(target=run1),
+        multiprocessing.Process(target=run2)
+    ]
+
+    # 启动进程
+    for process in processes:
+        process.start()
+
+    # 等待所有进程完成
+    for process in processes:
+        process.join()
+
+
+def multi_thread():
     # 创建线程
     threads = [
         threading.Thread(target=run1),
@@ -30,5 +45,8 @@ if __name__ == '__main__':
     # 等待所有线程完成
     for thread in threads:
         thread.join()
-
-    # print(datetime.datetime.now().strftime("%H:%M:%S:%f"))
+        
+    
+if __name__ == '__main__':
+    # multi_thread()
+    multi_process()
--- tests/test_mulProcess_mulThread/main_multi_process.py
+++ tests/test_mulProcess_mulThread/main_multi_process.py
@@ -2,17 +2,17 @@ import multiprocessing
 
 from utils import get_time
 
-import real_events.e2023_4.JuBaoPen4yue1_2023_4 as work1
-import real_events.e2023_4.JuBaoPen4yue2_2023_4 as work2
+import real_events.e2023_5.FuLiFengBao_TianShiWanOu as work1
+import real_events.e2023_5.FuLiFengBao_QBZ as work2
 
 
 def run1():
-    x = work1.mod("https://cf.qq.com/cp/a20230403three/pc/index.shtml?e_code=535546", get_time.get_next_2minute())
+    x = work1.mod("https://cf.qq.com/", get_time.get_next_minute())
     x.run_qiang_once(need_time=True)
 
 
 def run2():
-    x = work2.mod("https://cf.qq.com/cp/a20230403three/pc/index.shtml?e_code=535546", get_time.get_next_2minute())
+    x = work2.mod("https://cf.qq.com/", get_time.get_next_minute())
     x.run_qiang_once(need_time=True)
 
 
