b56a3d4c9501a8bc0aae180a06716f3aedde9f56
G3 <purep@qq.com>
2023-05-08 13:01:50 +0800

    增加5月聚宝盆抢AK，已抢到

--- real_events/e2023_5/main_single.py
+++ real_events/e2023_5/main_single.py
@@ -9,14 +9,13 @@ if platform.system() == 'Linux':
 from utils import get_time
 from base_class.AutoGet_ShenQi import AutoGet_ShenQi
 from base_class.AutoGet_Normal import AutoGet_Normal
-import real_events.e2023_5.FuLiFengBao_JinNiuZuo as work1
+import real_events.e2023_5.JuBaoPen as work1
 
-def test_AutoGet_Normal():
-    x = work1.mod("https://cf.qq.com/", get_time.get_next_hour_micro_early(800000))
+def test():
+    x = work1.mod("https://cf.qq.com/", get_time.get_next_hour_micro_early(900000))
     x.run()
     time.sleep(999999)
 
 
 if __name__ == '__main__':
-    # test_AutoGet_ShenQi()
-    test_AutoGet_Normal()
+    test()
--- tests/test_main_single.py
+++ tests/test_main_single.py
@@ -11,7 +11,7 @@ from base_class.AutoGet_ShenQi import AutoGet_ShenQi
 from base_class.AutoGet_Normal import AutoGet_Normal
 import test_FuLiFengBao_JinNiuZuo as work1
 
-def test_AutoGet_Normal():
+def test():
     x = work1.mod("https://cf.qq.com/", get_time.get_next_minute_micro_early(800000))
     x.run()
     time.sleep(999999)
@@ -19,4 +19,4 @@ def test_AutoGet_Normal():
 
 if __name__ == '__main__':
     # test_AutoGet_ShenQi()
-    test_AutoGet_Normal()
+    test()
